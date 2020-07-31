cbb2c02e86dfaadb38aaf210f1649b8a54addd6e
Manav Misra <manavm@visionwebsoft.com>
2020-07-31 14:43:11 -0500

    :recycle: Refactor FilterableProductTable into class

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,10 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from "react"
 
 import { ProductTable } from "./ProductTable"
 import { SearchBar } from "./SearchBar"
 
-export const FilterableProductTable = () => <Fragment>
-  <p>FilterableProductTable</p>
-  <ProductTable />
-  <SearchBar />
-</Fragment>
+export class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <p>FilterableProductTable</p>
+        <ProductTable />
+        <SearchBar />
+      </Fragment>
+    )
+  }
+}
