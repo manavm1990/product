b5c97a040a5470176fb675de4f18e9ca85804f28
Manav Misra <manavm@visionwebsoft.com>
2020-07-31 14:57:07 -0500

    :construction: Retrieve products and 'setState'

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -4,6 +4,19 @@ import { ProductTable } from "./ProductTable"
 import { SearchBar } from "./SearchBar"
 
 export class FilterableProductTable extends React.Component {
+  state = {
+    products: [],
+    searchText: "",
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/Claim-Academy-JS/products/products"
+    )
+
+    this.setState({ products: await res.json() })
+  }
+
   render() {
     return (
       <Fragment>
